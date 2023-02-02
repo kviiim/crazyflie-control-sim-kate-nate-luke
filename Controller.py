0db1f11216908cf84280b2f0b6e1b0e374e14f2d
kviiim <k82019@gmail.com>
2023-02-01 20:23:23 -0500

    part two maybe done

--- Controller.py
+++ Controller.py
@@ -32,8 +32,8 @@ class Controller1D():
         self.accumulated_error += error
 
         gain = 1
-        self.kp_z = .52
-        self.ki_z = 0.000001
+        self.kp_z = .57
+        self.ki_z = 0.001
         self.kd_z = -0.28
         p = self.kp_z* error
         i = self.ki_z*self.accumulated_error
